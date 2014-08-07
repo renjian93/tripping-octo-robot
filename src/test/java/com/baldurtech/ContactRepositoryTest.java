fa7f5cc24d3a130fec1110968e5ff05782743571
renjian93 <renjian93@gmail.com>
2014-08-07 10:22:59 +0800

    添加了ContacREpository的测试

---
+++ src/test/java/com/baldurtech/ContactRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.baldurtech;
+
+import com.baldurtech.unit.MiniatureSpiceTestCase;
+ 
+public class  ContactRepositoryTest extends MiniatureSpiceTestCase {
+    public void test_删除指定ID的Contact (){
+        
+    
+    }
+} 
