8437cd9e2b56feca9ff4f6f9e1c4ed6a97bd05ca
renjian93 <renjian93@gmail.com>
2014-08-08 10:58:31 +0800

    新建了ContactService和对应的测试案例类

---
+++ src/test/java/com/baldurtech/ContactServiceTest.java
@@ -0,0 +1,9 @@
+package com.baldurtech;
+
+import com.baldurtech.unit.MiniatureSpiceTestCase;
+
+public class ContactServiceTest extends MiniatureSpiceTestCase {
+    public void test_用户名为空时将不被保存() {
+    
+    }
+}
