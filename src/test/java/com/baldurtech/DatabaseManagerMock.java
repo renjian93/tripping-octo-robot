32e625b2afe60dd4a01f1364eb98f980faddc3b7
renjian93 <renjian93@gmail.com>
2014-08-08 11:00:41 +0800

    移动DatabaseManagerMock.java到对应的test目录下

--- src/test/java/com/baldurtech/DatabaseManagerMock.java
+++ src/test/java/com/baldurtech/DatabaseManagerMock.java
@@ -4,6 +4,7 @@ public class DatabaseManagerMock implements DatabaseManager {
     public String executeUpdateParam;
     public Object[] executeUpdateParam2;
     public int executeUpdateShouldReturn = 0;
+    
     public int executeUpdate(String sql, Object... params) {
         executeUpdateParam = sql;
         executeUpdateParam2 = params;
